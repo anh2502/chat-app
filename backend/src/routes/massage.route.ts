aea90eb7074d766f7eac407efc776b31e51c3f4d
anh2502 <nguyenkieuanhbl@gmail.com>
2024-07-10 17:57:50 +0700

    messages routes and controllers

--- backend/src/routes/massage.route.ts
+++ backend/src/routes/massage.route.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import protectRoute from "../middleware/protectRoute.js";
+import { sendMessage, getMessages, getUsersConnected } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-router.get("/conversations", (req, res)=>{
-    res.send("Conversation route")
-});
+router.get("/conversations", protectRoute, getUsersConnected)
+router.post("/send/:id", protectRoute, sendMessage)
+router.get("/:id", protectRoute, getMessages)
 
 export default router;
