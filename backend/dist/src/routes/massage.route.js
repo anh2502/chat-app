642523fcdcf7870ee9ac2b1ebae4626b32ab9fdf
anh2502 <nguyenkieuanhbl@gmail.com>
2024-07-12 16:19:51 +0700

    configuration for deployment

---
+++ backend/dist/src/routes/massage.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+import protectRoute from "../middleware/protectRoute.js";
+import { sendMessage, getMessages, getUsersConnected } from "../controllers/message.controller.js";
+const router = express.Router();
+router.get("/conversations", protectRoute, getUsersConnected);
+router.post("/send/:id", protectRoute, sendMessage);
+router.get("/:id", protectRoute, getMessages);
+export default router;
